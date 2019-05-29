1243b592326c45e6cfcd227e4dc49612ff7aefbc
misshalliday <50997461+misshalliday@users.noreply.github.com>
2019-05-29 22:00:05 +0100

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+//The main function is the starting point for all our Flutter apps.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
+      ),
+    ),
+  );
+}
